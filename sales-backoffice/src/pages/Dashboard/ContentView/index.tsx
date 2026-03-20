e6076270a22f551db7e6dbd36c1bc33f7ab5811c
Claudionor <netosousa2011@hotmail.com>
2026-03-19 23:47:35 -0300

    Revert "Merge pull request #1 from claudionor-sousa/feature/guilherme"
    This reverts commit 6c14b6b737a0aeda8e18b531cbc6e9bffbc49af2, reversing
    changes made to dd844e66214045b592d5dca9cc651464ef285f0b.

--- sales-backoffice/src/pages/Dashboard/ContentView/index.tsx
+++ sales-backoffice/src/pages/Dashboard/ContentView/index.tsx
@@ -1,18 +1,8 @@
-import { Autocomplete, TextField } from "@mui/material";
-import { top100Films } from "../Json";
-
-// import top100Films from './top100Films';
-
-
-
 
 export default function ContentViewDash(){
     return(
-       <Autocomplete
-        disablePortal
-        options={top100Films}
-        sx={{ width: 300 }}
-        renderInput={(params) => <TextField {...params} label="Movie" />}
-        />
+        <div >
+            Conteudo do dash
+        </div>
     );
 }
